e9e668746aa574dbaeee40664850ddbaf68e678b
David Miller <davidfmiller@me.com>
2017-02-07 11:17:02 -0700

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+  'use strict';
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      js: {
+        options: {
+          mangle: true
+        },
+        files: {
+          'docs/man.js' : ['src/man.js']
+        }
+      }
+    },
+
+    compass : {
+      dist : {
+        options : {
+          sassDir : 'src/',
+          cssDir : 'docs',
+          environment : 'production',
+          outputStyle : 'compressed'
+        }
+      }
+    },
+
+    watch : {
+      css : {
+        files : ['src/*.scss'],
+        tasks : ['compass']
+      },
+      js : {
+        files : ['src/*.js'],
+        tasks : ['uglify']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['compass']);
+};
